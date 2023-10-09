c4f98b419412d19dc1a98e1879c063dd68a54c25
Oleg Stepanischev <olegxx@gmail.com>
2023-10-09 20:01:29 +0300

    ClangSharp package

--- src/ClangSharp/ClangSharp.Interop/Extensions/CX_TemplateName.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) .NET Foundation and Contributors. All Rights Reserved. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
-// Ported from https://github.com/dotnet/clangsharp/blob/main/sources/libClangSharp
-
-namespace ClangSharp.Interop;
-
-public unsafe partial struct CX_TemplateName
-{
-    public CXCursor AsTemplateDecl => clangsharp.TemplateName_getAsTemplateDecl(this);
-}
--- src/ClangSharp/ClangSharp/IMergeable`1.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) .NET Foundation and Contributors. All Rights Reserved. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
-
-namespace ClangSharp;
-
-public interface IMergeable<TDecl>
-    where TDecl : Decl
-{
-}
